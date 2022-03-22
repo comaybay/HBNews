70493b868d29c2a0a76f1bcd071b99fdf4225d15
Nguyễn Văn Hiếu <19521510@gm.uit.edu.vn>
2022-03-22 10:09:27 +0700

    splashscreen

---
+++ app/src/main/java/com/cmb/hbnews/SplashScreen.kt
@@ -0,0 +1,21 @@
+package com.cmb.hbnews
+
+import android.app.ActivityOptions
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        val handler = Handler()
+        handler.postDelayed(Runnable {
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(this).toBundle())
+        }, 1600)
+
+
+    }
+}
