0efa1865abdb22faec5a21340471ec4e7573124c
Nguyễn Văn Hiếu <19521510@gm.uit.edu.vn>
2022-03-23 18:04:55 +0700

    giao diện đăng nhập đăng kí đã có mặt

---
+++ app/src/main/java/com/cmb/hbnews/Login.kt
@@ -0,0 +1,11 @@
+package com.cmb.hbnews
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/com/cmb/hbnews/SignUp.kt
@@ -0,0 +1,11 @@
+package com.cmb.hbnews
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
