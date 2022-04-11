fc2baceb97fb16a56ffab4ecf57399a7b282dd54
Thái Chí Bảo <comaybayy@gmail.com>
2022-04-11 20:05:57 +0700

    dùng viewPager2

--- app/src/main/java/com/cmb/hbnews/CategoryFragment.kt
+++ app/src/main/java/com/cmb/hbnews/CategoryFragment.kt
@@ -9,6 +9,7 @@ import androidx.fragment.app.FragmentStatePagerAdapter
 import com.cmb.hbnews.category.CategoryNewsHeadersViewPagerAdapter
 import com.cmb.hbnews.home.MainNewsListFragment
 import com.cmb.hbnews.scrapers.NewsCategory
+import com.google.android.material.tabs.TabLayoutMediator
 import kotlinx.android.synthetic.main.fragment_category.view.*
 
 /**
@@ -27,8 +28,21 @@ class CategoryFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_category, container, false)
 
-        view.viewPager.adapter = CategoryNewsHeadersViewPagerAdapter(getParentFragmentManager(), FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT)
-        view.tabLayout.setupWithViewPager(view.viewPager)
+        view.viewPager.adapter = CategoryNewsHeadersViewPagerAdapter(this)
+        TabLayoutMediator(view.tabLayout, view.viewPager, TabLayoutMediator.TabConfigurationStrategy { tab, position ->
+            when (position) {
+                0 -> tab.setText("TIN MỚI")
+                1 -> tab.setText("THỜI SỰ")
+                2 -> tab.setText("KINH DOANH")
+                3 -> tab.setText("THỂ THAO")
+                4 -> tab.setText("GIẢI TRÍ")
+                5 -> tab.setText("CÔNG NGHỆ")
+                6 -> tab.setText("ĐỜI SỐNG")
+                7 -> tab.setText("SỨC KHỎE")
+                8 -> tab.setText("DU LỊCH")
+                else -> throw NotImplementedError()
+            }
+        }).attach()
 
         return view
     }
--- app/src/main/java/com/cmb/hbnews/category/CategoryNewsHeadersViewPagerAdapter.kt
+++ app/src/main/java/com/cmb/hbnews/category/CategoryNewsHeadersViewPagerAdapter.kt
@@ -4,14 +4,16 @@ import android.util.Log
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.cmb.hbnews.scrapers.NewsCategory
 
-class CategoryNewsHeadersViewPagerAdapter(fm: FragmentManager, behavior: Int) : FragmentPagerAdapter(fm, behavior){
-    override fun getCount(): Int {
+class CategoryNewsHeadersViewPagerAdapter(fragment: Fragment) : FragmentStateAdapter(fragment){
+    override fun getItemCount(): Int {
         return NewsCategory.values().size
     }
 
-    override fun getItem(position: Int): Fragment {
+    override fun createFragment(position: Int): Fragment {
         return when(position) {
             0 -> CategoryNewsListFragment.newInstance(NewsCategory.LATEST)
             1 -> CategoryNewsListFragment.newInstance(NewsCategory.CURRENT_AFFAIRS)
@@ -22,21 +24,6 @@ class CategoryNewsHeadersViewPagerAdapter(fm: FragmentManager, behavior: Int) : 
             6 -> CategoryNewsListFragment.newInstance(NewsCategory.LIFESTYLE)
             7 -> CategoryNewsListFragment.newInstance(NewsCategory.HEALTH)
             8 -> CategoryNewsListFragment.newInstance(NewsCategory.TRAVEL)
-            else -> throw NotImplementedError()
-        }
-    }
-
-    override fun getPageTitle(position: Int): CharSequence? {
-        return when(position) {
-            0 -> "TIN MỚI"
-            1 -> "THỜI SỰ"
-            2 -> "KINH DOANH"
-            3 -> "THỂ THAO"
-            4 -> "GIẢI TRÍ"
-            5 -> "CÔNG NGHỆ"
-            6 -> "ĐỜI SỐNG"
-            7 -> "SỨC KHỎE"
-            8 -> "DU LỊCH"
             else -> throw NotImplementedError()
         }
     }
